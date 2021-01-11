d50c3671de4a586f1a8861d1332f5cf1b76d678c
eyssette <cedric.eyssette@gmail.com>
2021-01-11 02:21:20 +0100

    update

--- js/parse_data-textes.js
+++ js/parse_data-textes.js
@@ -3,7 +3,7 @@ var title;
 var content;
 var table_body;
 var table_body_start = document.getElementsByTagName("tbody")[0].innerHTML;
-Papa.parse("/data/textes.tsv", {
+Papa.parse("https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes.tsv", {
 	download: true,
 	header: false,
 	delimiter: "    ",
