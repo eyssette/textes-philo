ac247461b7780ce6adefc12225e6eb54f29759f5
eyssette <cedric.eyssette@gmail.com>
2021-01-15 03:39:43 +0100

    Parse Multiple TSV files

--- js/parse_data-textes.js
+++ js/parse_data-textes.js
@@ -1,15 +1,34 @@
 var rows;
+var allResults;
 var title;
 var content;
 var table_body;
 var table_body_start = document.getElementsByTagName("tbody")[0].innerHTML;
-Papa.parse("https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes.tsv", {
-	download: true,
-	header: false,
-	delimiter: "    ",
-	complete: function(results) {
-		rows = results.data;
-		rows = rows.splice(1, rows.length);
+
+var urls =  ['https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes-s1.tsv', 'https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes-s2.tsv', 'https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes-s3.tsv', 'https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes-bac.tsv', 'https://raw.githubusercontent.com/eyssette/textes-philo/main/data/textes-ce.tsv']
+
+Promise.all(
+  urls
+  .map(
+    url=>
+      new Promise(
+        (resolve,reject)=>
+          Papa.parse(
+            url,
+            {
+			  download: true,
+			  header: false,
+			  delimiter: "    ",
+              complete:resolve,
+              error:reject
+            }
+          )
+      )
+  )
+)
+.then(
+  function (results) {
+		rows=results[0].data.concat(results[1].data).concat(results[2].data).concat(results[3].data).concat(results[4].data);
 		table_body = "<tbody>";
 		rows.forEach((element) => {
 			recherche = document.getElementById("recherche_dans_le_sujet").value;
@@ -21,28 +40,36 @@ Papa.parse("https://raw.githubusercontent.com/eyssette/textes-philo/main/data/te
 			table_body = table_body + "</tr>";
 		});
 		table_body = table_body + "</tbody>";
-	}
-});
+  }
+)
+.catch(//log the error
+  err=>console.warn("Something went wrong:",err)
+)
+
 var input = document.getElementById("recherche_dans_le_sujet");
 input.oninput = handleInput;
 let checker = (arr, target) => target.every((v) => arr.includes(v));
 
 function handleInput(e) {
-	var search = input.value.toLowerCase();
+	var searchInput = input.value.toLowerCase();
 	if (this.timer) {
 		window.clearTimeout(this.timer);
 	}
 	this.timer = window.setTimeout(function() {
-		if (search) {
-		search_items = search.split("+");
+		if (searchInput) {
+		search_items = searchInput.split("+");
+
 		table_body = "<tbody>";
 		rows.forEach((element) => {
-			cellules = element[0].split("\t");
-			if (checker(cellules[0].toLowerCase(), search_items)) {
-				table_body = table_body + "<tr>";
-				cellules.forEach((cell) => {
-					table_body = table_body + "<td>" + cell + "</td>";
-				});
+				cellules = element[0].split("\t");
+				texte='« '+cellules[2]+' »';
+				if (checker(cellules.toString().toLowerCase(), search_items)) {
+					table_body = table_body + "<tr>";
+					auteur='<br/><b>'+cellules[0]+'</b>';
+					if(cellules[1]){reference=', '+cellules[1]} else{reference=''};
+					if(cellules[3]){precisions='<br/>'+cellules[3]} else{precisions=''};
+						table_body = table_body + "<td>" +texte+auteur+reference+precisions + "</td>";
+					
 				table_body = table_body + "</tr>";
 			}
 		});
