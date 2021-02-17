36526e80fd120adfac61df325ef1f48544c00997
eyssette <cedric.eyssette@gmail.com>
2021-02-18 00:18:14 +0100

    Update calcul-distances.R

--- R-scripts/calcul-distances.R
+++ R-scripts/calcul-distances.R
@@ -1,7 +1,6 @@
 library("readtext")
 library("dplyr")
 library("stringdist")
-library("svMisc")
 
 donnees=readtext("/Users/cedric.eyssette/Documents/GitHub/textes-philosophie/data/textes-FINAL.tsv", text_field="text", quote="", fill=TRUE)
 
@@ -13,20 +12,22 @@ calcul_distance <- function(x, y){
 
 matrice_distances=data.frame("Row1","Row2","Distance")
 
-
-for (i in 1:nrow(textes)) {
-  progress(i, progress.bar = TRUE)
-  Sys.sleep(0.01)x
-  row_i=slice(textes,i)
-  auteur_i=as.character(row_i[,2])
-  mots_auteur_i=strsplit(auteur_i," ")
-  for (j in 1:nrow(textes)) {
-    row_j=slice(textes,j)
-    auteur_j=as.character(row_j[,2])
-    mots_auteur_j=strsplit(auteur_j," ")
-    if (length(intersect(mots_auteur_i,mots_auteur_j)) > 0) {
-      matrice_distances=rbind(matrice_distances,list(i,j,calcul_distance(row_i[,1],row_j[,1])))    
-    }
+combinatoire=combn(nrow(textes),2, simplify=FALSE)
+for (i in 1:length(combinatoire)) {
+  combinatoire_i=strsplit(as.character(combinatoire[[i]])," ")
+  combinatoire_i1=as.numeric(combinatoire_i[1])
+  combinatoire_i2=as.numeric(combinatoire_i[2])
+  row1=slice(textes,combinatoire_i1)
+  row2=slice(textes,combinatoire_i2)
+  auteur1=as.character(row1[,2])
+  mots_auteur1=strsplit(auteur1," ")
+  auteur2=as.character(row2[,2])
+  mots_auteur2=strsplit(auteur2," ")
+  if (length(intersect(mots_auteur1,mots_auteur2)) > 0) {
+    matrice_distances=rbind(matrice_distances,list(combinatoire_i1,combinatoire_i2,calcul_distance(row1[,1],row2[,1])))
+  }
+  if(i %% 500==0) {
+    print(i)
   }
 }
 
